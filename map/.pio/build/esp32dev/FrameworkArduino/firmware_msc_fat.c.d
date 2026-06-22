89cc490c195958445f1e68fcb5f669efe94d5a23
tijmi <thdegroote18@gmail.com>
2026-06-22 11:00:02 +0200

    removed mess

--- map/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- /home/m8/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /home/m8/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h

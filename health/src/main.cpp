440d96475c6a5b76e12031ff8276241420e92a89
YorrickS <y.spexgoor@student.utwente.nl>
2026-06-23 12:07:07 +0200

    LED Strip implementation

--- health/src/main.cpp
+++ health/src/main.cpp
@@ -1,32 +1,52 @@
 #include <Arduino.h>
-#include <Fast>
+#include <FastLED.h>
 
-#define heartPin 4
-#define LEDPin 3
+#define HEART_PIN 4
+#define LED_PIN 3
+#define NUM_LEDS 60
 
+CRGB leds[NUM_LEDS]; // Make array of LEDs
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(heartPin, OUTPUT);
   pinMode(LEDPin, OUTPUT);
+
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(50); // 0–255
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    turnOnLeds(20);
+    // put your main code here, to run repeatedly:
     if (Serial.available() > 0) {
     String message = Serial.readStringUntil('\n');
     if (message.startsWith("DEATH")) {
-      String speedStr = message.substring(12); // Get the actual speed value from the message
-      int speed = speedStr.toInt();
+      String dataStr = message.substring(12); // Get the actual value from the message
+      int nrOfDeaths = dataStr.toInt();
+      nrOfDeaths = constrain(nrOfDeaths, 0, 60); // Clamp
 
-      int mappedSpeed = int(map(speed, 0, 5, 0, 255)); // Map to 255
-      mappedSpeed = constrain(mappedSpeed, 0, 255); // Clamp
+      turnOnLeds(nrOfDeaths);
 
-      // Acitvate pins
-      analogWrite(heartPin, mappedSpeed);
-      analogWrite(LEDPin, mappedSpeed);
+      int mappedDeaths = int(map(nrOfDeaths, 0, 60, 0, 255)); // Map to 255
+      mappedDeaths = constrain(mappedDeaths, 0, 255); // Clamp
+
+      // Acitvate heart
+      analogWrite(HEART_PIN, mappedDeaths);
       Serial.println("success");
     }
   }
+}
+
+void turnOnLeds(int count) {
+  // Clear all LEDs
+  FastLED.clear();
+
+  // Light up 'count' LEDs from index 0
+  for (int i = 0; i < count && i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red; // Change color as needed
+  }
+
+  FastLED.show();
 }

3ad226c08cdcda262a3ba4b163493af9e8baeb0b
YorrickS <y.spexgoor@student.utwente.nl>
2026-06-23 13:48:59 +0200

    Bug fixes

--- health/src/main.cpp
+++ health/src/main.cpp
@@ -4,37 +4,36 @@
 #define HEART_PIN 4
 #define LED_PIN 3
 #define NUM_LEDS 60
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
 CRGB leds[NUM_LEDS]; // Make array of LEDs
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(heartPin, OUTPUT);
-  pinMode(LEDPin, OUTPUT);
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(HEART_PIN, OUTPUT);
 
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  FastLED.setBrightness(50); // 0–255
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+    FastLED.setBrightness(50); // 0–255
+
+    turnOnLeds(20);
 }
 
 void loop() {
-    turnOnLeds(20);
     // put your main code here, to run repeatedly:
     if (Serial.available() > 0) {
     String message = Serial.readStringUntil('\n');
     if (message.startsWith("DEATH")) {
-      String dataStr = message.substring(12); // Get the actual value from the message
-      int nrOfDeaths = dataStr.toInt();
-      nrOfDeaths = constrain(nrOfDeaths, 0, 60); // Clamp
+      String dataStr = message.substring(6); // Get the actual value from the message
+      int nrOfDeaths = constrain(dataStr.toInt(), 0, 60);
 
       turnOnLeds(nrOfDeaths);
 
-      int mappedDeaths = int(map(nrOfDeaths, 0, 60, 0, 255)); // Map to 255
-      mappedDeaths = constrain(mappedDeaths, 0, 255); // Clamp
+      int mappedDeaths = int(map(nrOfDeaths, 0, 60, 0, 255), 0, 255); // Map to 255
 
       // Acitvate heart
       analogWrite(HEART_PIN, mappedDeaths);
-      Serial.println("success");
     }
   }
 }
